3333ea4fe12bdf1897112e1ade3c340e0a2ca7b6
lucas <lucas.lvn97439@gmail.com>
2021-12-06 14:35:26 +0400

    base

---
+++ ApiServer/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require("cors");
+
+// Server params
+const port        = process.env['SERVER_port']
+const corsOptions = {
+    origin: process.env['CORS_OPTIONS_ORIGIN']
+} 
+
+// Instanciate server
+const app     = express()
+app.use(cors(corsOptions));
+
+app.use(express.json());                      // Parsse request of content-type: application/json
+app.use(express.urlencoded({extended: true}))// Parse request of content-type: application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World')
+})
+
+
+app.listen(port, () => console.log('Server Started on http://localhost:3000'))
