170063382fb56f0d41145fe1e9b7a63bbb69dbeb
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:09:25 +0400

    AudiosController appended, to control all audio volume, play, pause, record

---
+++ app/src/components/AudiosController.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card dark color="#1A1D21" class="no-radius audios-controller">
+        <div class="d-flex audios-controller-content">
+            <VolumeController style="width: 200px" />
+
+            <div class="d-flex">
+                <v-btn color="#D81616" icon>
+                    <v-icon>mdi-record-circle-outline</v-icon>
+                </v-btn>
+
+                <v-btn color="#63D189" icon>
+                    <v-icon>mdi-play-circle-outline</v-icon>
+                </v-btn>
+
+                <v-btn color="#F24E1E" icon>
+                    <v-icon>mdi-pause-circle-outline</v-icon>
+                </v-btn>
+            </div>
+
+            <div class="timer">
+                1:00:00
+            </div>
+        </div>
+
+    </v-card>
+</template>
+
+<script src='./js/AudiosController.js' />
---
+++ app/src/components/js/AudiosController.js
@@ -0,0 +1,13 @@
+import VolumeController from '../VolumeController.vue'
+
+export default{
+    components: {
+        VolumeController
+    },
+
+    data(){
+        return{
+            timeRange: 10
+        }
+    }
+}
