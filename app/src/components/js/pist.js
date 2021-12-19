4c74f810d99ff60f39557ea60c656e7fb205f498
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:12:03 +0400

    Pist update

--- app/src/components/js/pist.js
+++ app/src/components/js/pist.js
@@ -1,5 +1,6 @@
 export default{
     name: 'pist',
+
     props:{
         currentTime:{required: true},
         audio: {required: true}
@@ -7,24 +8,14 @@ export default{
 
     data(){
         return{
-
+            type: '', // Audio || Record
         }
     },
 
     watch: {
-        currentTime(currentTime){
-           this.audio.audio.currentTime = currentTime
-        }
     },
 
     mounted(){
-        let audioContainer = document.getElementById('audioSlider' + this.audio.id)
-        this.audio.audio.addEventListener('click', () => {
-            console.log('input clicked');
-        })
-        console.log(this.audio.id);
-        audioContainer.appendChild(this.audio.audio)
-        this.audio.audio.setAttribute('controls', '')
     },
 
     methods: {
