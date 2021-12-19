ff64cb11dab0ba0e3d5af872ae1f4df4807bd39c
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:15:03 +0400

    Middleware for upload files appended to routes

--- ApiServer/apiRotuer.js
+++ ApiServer/apiRotuer.js
@@ -3,13 +3,15 @@ const AuthController = require('./controllers/AuthController')
 const SessionController = require('./controllers/SessionController')
 const PistController = require('./controllers/PistController')
 
+const MulterFilesManager = require('./middleware/MulterFileManager')
+
 exports.router = (() => {
     let apiRouter = express.Router()
 
     // Authentications routes
     apiRouter.route('/auth/').patch(AuthController.register)
     apiRouter.route('/auth/').post(AuthController.login)
-    apiRouter.route('/auth/').get(AuthController.test)  // Test le token
+    apiRouter.route('/auth/').get(AuthController.checkToken)  // Test le token
 
     // Sessions routes
     apiRouter.route('/sessions/create').post(SessionController.test)
@@ -17,9 +19,10 @@ exports.router = (() => {
     apiRouter.route('/sessions/delete').delete(SessionController.test)
 
     // Pists routes
-    apiRouter.route('/pists/').get(PistController.get)
-    apiRouter.route('/pists/update')
-    apiRouter.route('/pists/delete') 
+    // apiRouter.route('/pists/').get(PistController.get)
+    apiRouter.route('/pists/:id').get(PistController.get)
+    apiRouter.route('/pists/').post(MulterFilesManager.upload.single('audio'), PistController.import)
+    apiRouter.route('/pists/delete').delete(PistController.delete)
     apiRouter.route('/pists/search')
 
     //
