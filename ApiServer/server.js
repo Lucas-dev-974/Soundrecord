8e053aa7046ec226e4ffc72d569fc89e7c6f3aef
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:13:03 +0400

    server update

--- ApiServer/server.js
+++ ApiServer/server.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
-
 const express    = require('express')
 const cors       = require("cors");
 
-const apiRouter    = require('./apiRotuer').router
-const UploadsMidle = require('./middleware/UploadsMid').upload
-const JwtMidle     = require('./middleware/Jwt').isAuthorized
+const apiRouter = require('./apiRotuer').router
+const JwtMidle  = require('./middleware/Jwt').isAuthorized
+
 
 // Server params
 const port        = process.env.SERVER_PORT
@@ -13,19 +12,16 @@ const corsOptions = {
     origin: process.env.CORS_OPTIONS_ORIGIN
 } 
 
-
 // Instanciate server
 const server     = express()
+server.use(JwtMidle)
+
 server.use(cors(corsOptions));
-
-// Tell to the serve we want parse json and url-encoded from request
 server.use(express.json());
-server.use(express.urlencoded({extended: true}));
-
+server.use(express.urlencoded({extended: true}))
 
 // Setup Middleware
-server.use(JwtMidle)
-server.use(UploadsMidle)
+
 
 // Handle Api Routes
 server.use('/api/', apiRouter)
