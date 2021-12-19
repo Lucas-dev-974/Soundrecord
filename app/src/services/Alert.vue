1be0b7d7102c78029edca4b19f6844f1cf838b0e
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:07:08 +0400

    Setup Alert service

---
+++ app/src/services/Alert.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div v-for="alert in alerts" :key="alert.id">
+            <v-alert type="primary" v-model="alert.open"  transition="scale-transition" style="width: fit-content; ">
+                <v-row >
+                    <v-col cols="11" style="padding: 0 !important">
+                        <div  v-if="Array.isArray(alert.message)">
+                            <div  v-for="message in alert.message" :key="message">
+                                {{message}}
+                            </div>
+                        </div>
+                        <div v-else>
+                            {{alert.message}}
+                        </div>
+                    </v-col>
+                    <v-col cols="1"  style="padding: 0 !important">
+                        <v-btn color="success" icon @click="removeAlert(alert)">
+                            <v-icon>mdi-close</v-icon>
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-alert>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            alerts: this.$store.state.alerts,
+        }
+    },
+
+    methods: {
+        removeAlert: function(alert){
+            console.log(alert);
+            alert.open = false
+            this.$store.commit('remove_alert', alert.id)
+        }
+    }
+}
+</script>
