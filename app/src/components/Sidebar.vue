97b3c968ffab5304c0c78d1a218feab71d31e705
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:10:27 +0400

    Sidebar to show all pist imported in session appended

---
+++ app/src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card width="fit-content" height="100%" dark class="no-radius" >
+        <v-navigation-drawer color="#1A1D21" dark left :mini-variant.sync="mini" v-model="open" permanent>
+            <v-list-item>
+                <Bibi />
+                <v-list-item-title>Pistes</v-list-item-title>
+
+                <v-btn icon @click.stop="mini = !mini">
+                    <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+            </v-list-item>
+        </v-navigation-drawer>
+    </v-card>
+</template>
+
+<script src='./js/sidebar.js' />
---
+++ app/src/components/js/sidebar.js
@@ -0,0 +1,19 @@
+import Bibi from '../bibi.vue'
+
+export default{
+    components: {
+        Bibi
+    },
+
+    data(){
+        return{
+            open: true,
+            mini: true
+        }
+    },
+
+
+    methods: {
+
+    }
+}
