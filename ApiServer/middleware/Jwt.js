77a0412dc4ae3ea4d68151ae3969351f8bb853e6
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:23:44 +0400

    Function checkToken added

--- ApiServer/middleware/Jwt.js
+++ ApiServer/middleware/Jwt.js
@@ -11,32 +11,38 @@ module.exports = {
     },
 
     parseauthorization: function(authorization){
-        return (authorization != null || authorization !== "Bearer undefined") ? authorization.replace('Bearer ', '') : null
+        if(typeof(authorization) == 'undefined') return null
+        else return authorization.replace('Bearer ', '')
     },
 
     isAuthorized: function(req, res, next){
         let headerAuth = req.headers['authorization']
         let token = module.exports.parseauthorization(headerAuth)
+        if(req.query.token) token = req.query.token // Si le token est spécifié dans l'url via http://url...?token=ferjnfiejrn
 
-        if(req.path !== "/api/auth/"){
+
+        if(req.path !== "/api/auth/"){ // Authorize l'accès au controller si c'est pour l'authentification
             if(token !== null){
-                try{
-                    let jwtToken = jwt.verify(token, JWT_SIGN_SECRET)
-
-                    if(jwtToken !== null){
-                        req.userID = jwtToken.userID
-                    }
-                }catch(err){
-                    console.log(err);
-                    return res.status(401).json({'error': 'Votre connexion n\'est plus valable ! Reconnectez-vous.'})
-                }
+                let tokenInfos = module.exports.checkToken(token)
+                if(tokenInfos.error) res.status(403).json(tokenInfos)
+                req.userID = tokenInfos.userID
             }else{
                 return res.status(401).json({
-                    error: "Token non Autorisée, veuillez vou connecté !"
+                    error: "Aucun Token renseigner veuillez vous connectez !"
                 })
             }
             next()
         }else next()
-
     },
+
+    checkToken: function(token){
+        try{
+            let jwtToken = jwt.verify(token, JWT_SIGN_SECRET)
+            if(jwtToken !== null){
+                return jwtToken
+            }
+        }catch(err){
+            return {error: 'Votre connexion n\'est plus valable ! Reconnectez-vous.'}
+        }
+    }
 }
