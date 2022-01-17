dbc9575e229176f048023321adb17ad8ac8bb39f
lucas <lucas.lvn97439@gmail.com>
2022-01-17 11:51:02 +0400

    filterextension file update to handle many extensions

--- ApiServer/middleware/MulterFileManager.js
+++ ApiServer/middleware/MulterFileManager.js
@@ -6,18 +6,22 @@ const StoragePath = path.resolve(__dirname, '..')
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        let UserDirname
+        let UserDirname // Repository where to import all file for the user
+        
         switch(req.path){
-            case '/pists/':
+            case '/pists/': // if path of the route is /api/pist/ 
+                // We want to difine where to import the file
                 UserDirname = StoragePath + '/public/' + 'user-' + req.userID + '/imported/'
                 break
             default:
                 return cb(new Error('Impossible d\'importer'))
         }
 
-        req.fileInfos = file
+        // let us to use the data file in the controller
+        req.fileInfos = file    
         req.fileInfos['date'] = Date.now()
 
+        // If the folder does'nt exist create one
         if (!fs.existsSync(UserDirname)) fs.mkdirSync(UserDirname)
         cb(null, UserDirname)
     },
@@ -30,11 +34,20 @@ let storage = multer.diskStorage({
 
 exports.upload = multer({
     storage: storage,
+
+    // Upload only if file is
     fileFilter: function(req, file, callback){
         let ext = path.extname(file.originalname)
-        if(ext !== '.mp3'){
+        
+        let extensions = ['.mp3']
+        let error = false
+
+        extensions.forEach(ext => {
+            if(ext !== ext) error = true
+        })
+        if(error == true){
             req.isNotAudio = true
-            callback(null, false)
+            callback(null, false) 
         } 
         else callback(null, true)
     }
