6c4e16fc9235cca2320faa53af4e63fe98fc12a8
lucas <lucas.lvn97439@gmail.com>
2022-01-17 08:57:44 +0400

    code update

---
+++ ApiServer/migrations/20211207095356-create-session.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Sessions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userid: {
+        type: Sequelize.INTEGER,
+      },
+      session_name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Sessions');
+  }
+};
