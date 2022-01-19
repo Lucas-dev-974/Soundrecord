6f3f2f568ed5f365a7739295c28aba993c8a5289
lucas <lucas.lvn97439@gmail.com>
2022-01-19 08:30:51 +0400

    code update

--- ApiServer/middleware/Jwt.js
+++ ApiServer/middleware/Jwt.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const JWT_SIGN_SECRET = process.env.JWT_SIGN_SECRET
+const models = require('../models/')
 
 module.exports = {
     generateToken: function(user){
@@ -15,33 +16,31 @@ module.exports = {
         else return authorization.replace('Bearer ', '')
     },
 
-    isAuthorized: function(req, res, next){
+    isAuthorized: async function(req, res, next){
         let headerAuth = req.headers['authorization']
         let token = module.exports.parseauthorization(headerAuth)
         if(req.query.token) token = req.query.token // Si le token est spécifié dans l'url via http://url...?token=ferjnfiejrn
 
         
         // if(req.path !== "/api/auth/"){ // Authorize l'accès au controller si c'est pour l'authentification
-        //     if(token !== null){
-        //         let tokenInfos = module.exports.checkToken(token)
-        //         if(tokenInfos.error) res.status(403).json(tokenInfos)
-        //         req.userID = tokenInfos.userID
-        //     }else{
-        //         return res.status(401).json({
-        //             error: "Aucun Token renseigner veuillez vous connectez !"
-        //         })
-        //     }
+     
         //     next()
         // }
+        if(req.path == "/api/auth/" && req.method !== 'GET') return next()
+
         if(token !== null){
             let tokenInfos = module.exports.checkToken(token)
             if(tokenInfos.error) res.status(403).json(tokenInfos)
             req.userID = tokenInfos.userID
+
+            let user = await models.User.findByPk(tokenInfos.userID)
+            if(!user) return res.status(403).json('Votre compte persoit un problèment. L\'accèes y est impossible pour le moment !')
         }else{
             return res.status(401).json({
                 error: "Aucun Token renseigner veuillez vous connectez !"
             })
         }
+        
         next()
     },
 
