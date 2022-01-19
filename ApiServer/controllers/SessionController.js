934c123a2db0ccbd7db3373fc4db8ae6d6026325
lucas <lucas.lvn97439@gmail.com>
2022-01-19 08:19:54 +0400

    get function updated to when get one session return importedPist in the session and session Text

--- ApiServer/controllers/SessionController.js
+++ ApiServer/controllers/SessionController.js
@@ -1,32 +1,52 @@
 const models = require('../models')
 const validator = require('../validator')
 
+const {returnFields} = require('../utils.js')
+
 module.exports = {
     get: async function(req, res){ 
-        // retourne la session qui à été modifié en dernier si pas de sessions id en parametre 
+        // return the current session if id params is given
 
-        let id = req.query.id ?? null
-        if(id !== null && isNaN(id)) return res.status(400).json({error: 'L\'id renseigner est incorrect !'})
-        else if(id == null){
-            let session = await models.Session.create({
+        // let id = req.query.id ?? null // For axios request
+        let id = req.body.id ?? null     // For postman request
+
+        // if id is not number return error
+        if(isNaN(id) == true) return res.status(400).json({error: 'L\'id renseigner est incorrect !'})
+        else if(id == null){    // IF NO ID, CREATE NEW SESSION WITH TEXT
+            let session = await models.Session.create({ // Create Session
                 userid: req.userID,
                 session_name: 'Untilted',
             })
 
+            session = returnFields(session.dataValues, ['id', 'userid', 'session_name'])
+
+            // Create text
+            let text = await models.Text.create({ text: '', sessionid: session.id }).catch(error => { console.log(error) })
+
+            session['text'] = returnFields(text.dataValues, ['text', 'id'])
             return res.status(200).json(session)
         }
 
-        let session = await models.Session.findByPk(id);
-        return res.status(200).json(session)
+        let session = await models.Session.findOne({ where: {id: id}, include: [models.Text, models.ImportedInProject] })
+        console.log(session.dataValues.Text);
+        text     = returnFields(session.dataValues.Text.dataValues, ['id', 'text'])
+        session  = returnFields(session.dataValues, ['id', 'session_name', 'userid']);     
+
+        // Check if token userID is the userid via database
+        if(session.userid !== req.userID) return res.status(403).json({error: 'Vous devez avoir créer cet session pour la modifier !'})
+
+        // Get all imported pist for the session
+        let importedIn = await models.ImportedInProject.findAll({
+            where: {sessionid: id},
+            attributes: ['sessionid', 'importid', 'id', 'userid']
+        }).catch(error => { console.log(error); })
+
+        if(!importedIn) return res.status(400).json({'error': 'Une erreur est survenue veuillez réesayer plus tard !'})
         
-    },
-
-    get_all: function(req, res){
-
-    },
-
-    update: function(req, res){
-
+        session['text'] = text
+        session['importedIn'] = importedIn
+        
+        return res.status(200).json(session)
     },
 
     delete: function(req, res){
@@ -60,5 +80,11 @@ module.exports = {
         console.log(validated.data_name);
         let data_name = validated.data_name.split('|')
         console.log('data_name: ', data_name);
+    },
+    
+    import: function(req, res){
+        let validated = validator.validate(req.body, {
+            'id': 'int'
+        })
     }
 }
