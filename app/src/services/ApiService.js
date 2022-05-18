a0fc9009579576d0e4eef1a5715ddda0890fa482
lucas <lucas.lvn97439@gmail.com>
2022-05-18 18:14:17 +0400

    api service update

--- app/src/services/ApiService.js
+++ app/src/services/ApiService.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'http://localhost:3000',
     headers: {
-        'Authorization': 'Bearer ' + JSON.parse(window.localStorage.getItem('vuex')).token  ?? ''
+        'Authorization': 'Bearer ' + JSON.parse(window.localStorage.getItem('vuex')) ? JSON.parse(window.localStorage.getItem('vuex')).token : ''
     },
 })
