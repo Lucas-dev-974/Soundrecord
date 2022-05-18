d08d68b1e85c21fcec5f870627bda05de00949ea
lucas <lucas.lvn97439@gmail.com>
2022-05-18 18:20:09 +0400

    app update

--- app/src/services/ApiService.js
+++ app/src/services/ApiService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-console.log('okokokok', JSON.parse(window.localStorage.getItem('vuex')) );
+console.log('okokokok', JSON.parse(window.localStorage.getItem('vuex')));
+
 export default axios.create({
     baseURL: 'http://localhost:3000',
     headers: {
