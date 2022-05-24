3a9a32896a9ecd7c095abb287a673bcc7b6d48d7
lucas <lucas.lvn97439@gmail.com>
2022-05-24 21:36:42 +0400

    artisti.vue .js render update fonctions update

--- app/src/components/discover/js/artist.js
+++ app/src/components/discover/js/artist.js
@@ -1,30 +1,41 @@
 import api from "../../../services/ApiService"
+import Searchbar from '../../Searchbar.vue'
 
 export default{
     name: 'artist',
+    components: { Searchbar },
+
     data() {
         return {
-            artist: [],
+            artists: [],
+            art_selected: 1,
             query_search: '',
+
+            api: api
         }
     },
 
-    mounted() {
-        this.get_artist()
+    async mounted() {
+        console.log(api.defaults.baseURL);
+        await this.get_artists()
     },
 
     methods: {
-        get_artist: function(){
-            api.get('/api/artists?token=' + this.$store.state.token ?? '').then(({data}) => {
-                console.log(data);
-            }).catch(error => {
-                console.log(error);
-            })
+        get_artists: async function(page = 0){
+            let response = await api.get('/api/creators?page=' + page + '&token=' + this.$store.state.token ?? '').catch(error => {console.log(error)})
+            if(response.status == 200)
+                this.artists = response.data.rows
+            
+            return this.artists
+            
         },
 
         like: function(id){
             api.post('/like', {model: 'creator', modelid: id}).theb(({data}) => {
                 console.log(data);
+                this.artist.filters(artist => {
+                    if(artist.id == id) artist.liked = data.liked
+                })
             }).catch(error => {
                 console.log(error);
             })
