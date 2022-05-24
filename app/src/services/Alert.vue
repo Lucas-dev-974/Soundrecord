b08304ffa74ee5d6500c1535a0b7749c21d7a0a1
lucas <lucas.lvn97439@gmail.com>
2022-05-24 21:31:19 +0400

    Alert render update

--- app/src/services/Alert.vue
+++ app/src/services/Alert.vue
@@ -1,44 +1,41 @@
 <template>
     <div>
-        <v-alert elevation='15' :type="alert.type" v-model="alert.open"  transition="scale-transition" style="width:720px " class="mx-auto">
-            <v-row >
-                <v-col cols="11" >
-                    <div  v-if="Array.isArray(alert.message)">
-                        <div v-for="message in alert.message" :key="message">
-                            {{message}}
-                        </div>
+        <v-alert elevation='15' :type="alert.type" v-model="alert.open"  transition="scale-transition" style="width: fit-content;" class="mx-auto -10 d-flex align-center">
+            <div class="alert-content mr-10">
+                <div v-if="Array.isArray(alert.message)">
+                    <div v-for="message in alert.message" :key="message">
+                        {{message}}
                     </div>
-                    <div v-else>
-                        {{alert.message}}
-                    </div>
-                </v-col>
-                <v-col cols="1"  class="ma-0 pa-0 pt-1">
-                    <v-btn color="red"  icon @click="removeAlert(alert)">
-                        <v-icon>mdi-close</v-icon>
-                    </v-btn>
-                </v-col>
-            </v-row>
+                </div>
+                <div v-else>
+                    {{alert.message}}
+                </div>                       
+            </div>
+    
+            <v-btn color="red" class="ma-0 pa-0" style="right: 5px; position: absolute; top: 10px;"  icon @click="removeAlert(alert)">
+                <v-icon>mdi-close</v-icon>
+            </v-btn>
         </v-alert>
     </div>
 </template>
 
 <script>
 export default{
+    name: 'alert_service',
     props: {
         alert: {required: true}
     },
 
     mounted(){
-        console.log('okok');
         setTimeout(() => {
-            this.$store.commit('remove_Alert', this.alert.id)
+            this.$store.commit('remove_alert', this.alert.id)
         }, 10000)
     },
 
     methods: {
         removeAlert: function(alert){
             alert.open = false
-            this.$store.commit('remove_Alert', alert.id)
+            this.$store.commit('remove_alert', alert.id)
         }
     }
 }
