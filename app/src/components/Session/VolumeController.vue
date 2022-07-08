6ef3509b1fba05c0280db26822ef825dd15e8611
lucas <lucas.lvn97439@gmail.com>
2022-07-08 22:43:13 +0400

    volume controller class update

--- app/src/components/Session/VolumeController.vue
+++ app/src/components/Session/VolumeController.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="d-flex  w-100" >
-        <v-slider v-model="value" step="0" max=100 prepend-icon="mdi-volume-high" hide-details="true"></v-slider>
+    <div class="d-flex align-center  w-100" >
+        <v-slider dense v-model="value" step="0" max=100 prepend-icon="mdi-volume-high" hide-details="true"></v-slider>
     </div>
 </template>
 
