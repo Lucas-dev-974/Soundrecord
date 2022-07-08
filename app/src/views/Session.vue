d95bbc5be1e1766a613f3b194fb02a5d83e416f0
lucas <lucas.lvn97439@gmail.com>
2022-07-08 22:27:56 +0400

    upadte barmanager on session page add skeleton component

--- app/src/views/Session.vue
+++ app/src/views/Session.vue
@@ -2,15 +2,13 @@
     <div style="height: 100%" class="d-flex">
         <div class="w-100">
             <v-btn color="success" x-small outlined style="position: absolute; top: 10px; right: 10px; z-index: 90" @click="save">Sauvegarder</v-btn>
-            <BarManager />
+            <BarManager v-if="$store.state.width > 600"/>
             <div class="d-flex w-100" style="height: calc(100vh - 80px)">
                 <Sidebar v-if="track_loaded"/>
                 <PistContainer />
             </div>
 
-            <v-sheet color="grey darken-2" class="pa-3" :style="{display: track_loaded ? 'block' : 'block'}">
-                <v-skeleton-loader class="mx-auto" max-width="1200" height="500px" type="card"></v-skeleton-loader>
-            </v-sheet>
+            <BarManager v-if="$store.state.width < 600"/>
         </div>
     </div>
 </template>
