dc24e1db724e17ab81f32273a71814f8480d8349
lcs lvn <lucas.lvn97439@gmail.com>
2022-07-12 07:26:47 +0200

    api service error update on get token

--- app/src/services/ApiService.js
+++ app/src/services/ApiService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+const token = window.localStorage.getItem('vuex') ? (JSON.parse(window.localStorage.getItem('vuex')).token ?? null) : null
 export default axios.create({
     baseURL: 'http://localhost:3000',
     headers: {
-        'Authorization': 'Bearer ' + JSON.parse(window.localStorage.getItem('vuex')) ? JSON.parse(window.localStorage.getItem('vuex')).token : ''
+        'Authorization': 'Bearer ' + token,
     },
-})
+});
