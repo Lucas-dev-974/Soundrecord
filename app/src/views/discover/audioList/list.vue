6c7533bfe9d5efd46ff22f8cca7510a51a209eed
lucas <lucas.lvn97439@gmail.com>
2023-10-08 00:59:05 +0400

    fix grid style

--- app/src/views/discover/audioList/list.vue
+++ app/src/views/discover/audioList/list.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="grid grid-x-6 audio-collection">
+    <ul class="grid audio-collection">
         <AudioItem :audio="audio" v-for="audio in audios" :key="audio.id" />
     </ul>
 </template>
