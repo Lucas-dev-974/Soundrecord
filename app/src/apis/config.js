e73abb5b2a18f3051f5f1a5050dfd6a7f89c45e2
lucas <lucas.lvn97439@gmail.com>
2023-10-28 09:19:53 +0400

    update use of api service

--- app/src/apis/config.js
+++ app/src/apis/config.js
@@ -4,11 +4,18 @@ const token = window.localStorage.getItem("vuex")
   ? JSON.parse(window.localStorage.getItem("vuex")).token
   : null;
 
+const host = () => {
+  if (!window.location.href.includes("localhost")) {
+    return "http://api.soudrecord.lelu0920.odns.fr/";
+  } else {
+    return "http://localhost:3000/api";
+  }
+};
 const config = {
-    baseURL: "http://localhost:3000/api",
-    headers: {
-      Authorization: "Bearer " + token,
-    },
-  };
+  baseURL: host() + "api",
+  headers: {
+    Authorization: "Bearer " + token,
+  },
+};
 
 export default axios.create(config);
