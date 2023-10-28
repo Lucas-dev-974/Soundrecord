b839fd18a874c0bfb949623b4f2ee9998ca7a596
lucas <lucas.lvn97439@gmail.com>
2023-10-28 09:52:08 +0400

    cors update

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const { publicPath } = require("./middleware/PublicPath");
 const { checkPagingParams } = require("./middleware/Paging");
-const MediaManager = require("./middleware/MediaManager").handleMediaUrl;
 
 const router = require("./api-routes").router;
 const adminRoutes = require("./routes/admin.routes").router;
@@ -11,27 +10,21 @@ const adminRoutes = require("./routes/admin.routes").router;
 const JwtMidle = require("./middleware/Jwt").isAuthorized;
 // Server params
 const port = process.env.SERVER_PORT;
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-};
 
 // Instanciate server
 const server = express();
-
-// * ... Import ?
-
-// Setup Middleware
+server.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  next();
+});
 server.use(cors());
-// server.use(MediaManager)
 server.use(JwtMidle);
 server.use(publicPath);
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(checkPagingParams);
-// Handle Api Routes
-server.use("/api/", router);
 
+server.use("/api/", router);
 server.use("/api/admin/", adminRoutes);
 
 server.listen(port, () => console.log("http://localhost:" + port));
